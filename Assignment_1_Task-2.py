1f7731c99288e26fdec3b4fa7de5ff0e88e37c49
Abhishek-1988-coder <sahoo.abhishek2013@gmail.com>
2025-04-07 11:24:56 +0530

    Add files via upload

---
+++ Assignment_1_Task-2.py
@@ -0,0 +1,7 @@
+######################### TASK-2 #######################
+
+a=input("Enter your first name: ")
+b=input("Enter your second name: ")
+print("Hello,"+ " "+ a +" " +b+ "!"+ " Welcome to python program.")
+
+####################### END OF TASK-2 ##################
